0d5d75b115768e807e70f3760d90515476386c00
Aonodensetsu <remok99999@gmail.com>
2026-01-12 13:34:46 +0100

    podsumowanie

---
+++ media/lab.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cat >laboratorium.txt <<'END'
+
+Treść poniżej komendy cat znajdzie się w pliku.
+Podana w linii komendy nazwa stanowi nazwę utworzonego pliku.
+Znacznik END zakańcza treść pliku i pozwala na uruchomienie dalej zawartch komend.
+
+END
