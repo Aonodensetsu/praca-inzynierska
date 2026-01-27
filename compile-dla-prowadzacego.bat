04aecf357548ad0f5090967f428e5bff48d44dbe
Aonodensetsu <remok99999@gmail.com>
2026-01-27 17:29:36 +0100

    wyczyszczenie wersji lokalnej

--- compile-dla-prowadzacego.bat
+++ compile-dla-prowadzacego.bat
@@ -1,2 +1,1 @@
 latexmk dla-prowadzacego.tex
-scp out/dla-prowadzacego.pdf srv:/home/ao/.local/docker/home-lab/nginx/sites/dot/misc/inzynierka-polaczenia.pdf
--- compile.bat
+++ compile.bat
@@ -1,2 +1,1 @@
 latexmk main.tex
-scp out/main.pdf srv:/home/ao/.local/docker/home-lab/nginx/sites/dot/misc/inzynierka.pdf
